496fb55ecc7e8f79e621f1050a0837d088eee6f6
dhananjay-gupta3 <102008241+dhananjay-gupta3@users.noreply.github.com>
2025-08-13 14:44:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import employee from './routes/employee.js'
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
+    origin: https://employees-n9be.onrender.com || 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true
 }));
